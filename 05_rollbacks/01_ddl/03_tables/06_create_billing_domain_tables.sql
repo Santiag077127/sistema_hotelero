4e209a8d250c727d909c79fee5166cf72f1abf15
Santiag077127 <santiagogordoperez77@gmail.com>
2026-05-13 16:01:36 -0500

    feact:(The billing domain tables were added.).

---
+++ 05_rollbacks/01_ddl/03_tables/06_create_billing_domain_tables.sql
@@ -0,0 +1,5 @@
+-- Drop billing domain
+DROP TABLE IF EXISTS invoice_detail CASCADE;
+DROP TABLE IF EXISTS partial_payment CASCADE;
+DROP TABLE IF EXISTS invoice CASCADE;
+DROP TABLE IF EXISTS proforma_invoice CASCADE;
