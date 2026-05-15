0efd769618cb15fa3b2d0f6d63db494830338d59
Yerson Rubiano <yersonstivencuellarrubiano@gmail.com>
2026-05-15 12:36:08 -0500

    feact:(The notifications domain tables are added.)

---
+++ 05_rollbacks/01_ddl/03_tables/07_create_notificacion_domain_tables.sql
@@ -0,0 +1,5 @@
+-- Drop notification domain
+DROP TABLE IF EXISTS customer_loyalty CASCADE;
+DROP TABLE IF EXISTS term_condition CASCADE;
+DROP TABLE IF EXISTS alert CASCADE;
+DROP TABLE IF EXISTS promotion CASCADE;
